b742c2f610f9a9a97e31f760a0777f3b274138c0
bosxch <amparocardosobosch@gmail.com>
2023-07-11 19:00:20 -0300

    estilos basicos de login con module.css, preventivos

--- front/src/components/logIn/logIn.jsx
+++ front/src/components/logIn/logIn.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
-import { useDispatch } from 'react-redux'
+import { useDispatch } from "react-redux";
 import { loginUser } from "../../redux/actions/actions";
+import style from './login_styles.module.css'
+
+ //falta onClick a /register en su respectivo botón
+
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -10,22 +14,37 @@ const Login = () => {
     e.preventDefault();
     dispatch(loginUser({ email, password }));
   };
+
   return (
-    <form onSubmit={handleSubmit}>
+    <div className={style.form_container}>
+    <div className={style.title}>Bienvenido</div>
+    <div className={style.subtitle}>Ingrese!</div>
+    <form onSubmit={handleSubmit} className={style.form_style}>
+    <div className={style.input_cont01}>
+        <label className={style.labeltxt}>Email</label>
       <input
         type="email"
         placeholder="Email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
+        className={style.input}
       />
+      </div>
+      <div className={style.input_cont02}>
+      <label className={style.labeltxt}>Contraseña</label>
       <input
         type="password"
-        placeholder="Password"
+        placeholder="Contraseña"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
+        className={style.input}
       />
-      <button type="submit">Login</button>
+      </div>
+      <button className={style.button_login} type="submit">Login</button>
     </form>
+    <hr className={style.hr}/>
+    <button className={style.register}>Regístrate</button>
+    </div>
   );
 };
 export default Login;
