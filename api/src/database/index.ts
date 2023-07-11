627b3bb11f13aa10b52141afba93a8cac87fefb1
Santy1422 <102694038+Santy1422@users.noreply.github.com>
2023-07-11 17:29:06 +0200

    Update index.ts

--- api/src/database/index.ts
+++ api/src/database/index.ts
@@ -1,18 +1,15 @@
-import mongoose from "mongoose"
-import  {MONGO_URI} from "../config/env"
-import usersSchema from "./schemas/usersSchema";
-mongoose.set('strictQuery', true); //ver sies necesario
+const mongoose = require("mongoose");
+const { MONGO_URI } = require("../config/env");
+const usersSchema = require("./schemas/usersSchema");
+
+mongoose.set('strictQuery', true); // ver si es necesario
+
 const conn = mongoose.createConnection(MONGO_URI);
 
+const users = conn.model("users", usersSchema);
+const products = conn.model("products", require("./schemas/productsSchema"));
+const providers = conn.model("providers", require("./schemas/providersSchema"));
+const clients = conn.model("clients", require("./schemas/clientsSchema"));
+const imports = conn.model("imports", require("./schemas/importsSchema"));
 
-export const users= conn.model("users", usersSchema)
-export const products= conn.model("products", require("./schemas/productsSchema"))
-export const providers= conn.model("providers", require("./schemas/providersSchema"))
-export const clients= conn.model("clients", require("./schemas/clientsSchema"))
-export const imports= conn.model("imports", require("./schemas/importsSchema"))
-
-
-
-//characters.find().populate("films", ["_id", "title"]).then(log);
-
- 
+module.exports = { users, products, providers, clients, imports };
