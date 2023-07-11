632b488d6ecd9130b9e9eafe0c187f4730a2e1f3
bosxch <amparocardosobosch@gmail.com>
2023-07-11 17:11:18 -0300

    actions not tried yet

--- front/src/redux/actions/actions.js
+++ front/src/redux/actions/actions.js
@@ -13,3 +13,51 @@ export const getUser = () => {
     dispatch({ type: GET_USER, payload: userData.data[0] });
   };
 };
+
+export const editUser = ({ id }) => {
+    return async function (dispatch) {
+      try {
+        const editUser = await axios.put("user/edit", { id });
+        dispatch({ type: EDIT_USER, payload: editUser.data.password });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  };
+
+  export const loginUser = ({ email, password }) => {
+    return async function (dispatch) {
+    try {
+    const loginUser = await axios.post("user/login", { email, password });
+    dispatch({ type: LOGIN_USER, payload: loginUser.data });
+    } catch (error) {
+    console.log(error);
+        }};
+    };
+    export const registerUser = ({ email, password }) => {
+    return async function (dispatch) {
+    try {
+    const registerUser = await axios.post("user/register", { email, password });
+    dispatch({ type: REGISTER_USER, payload: registerUser.data });
+    } catch (error) {
+    console.log(error);
+        }};
+    };
+    export const recoverUser = ({ email }) => {
+    return async function (dispatch) {
+    try {
+    const recoverUser = await axios.post("user/recover", { email });
+    dispatch({ type: RECOVER_USER, payload: recoverUser.data });
+    } catch (error) {
+    console.log(error);
+        }};
+    };
+    export const byEmailUser = ({ email }) => {
+    return async function (dispatch) {
+    try {
+    const byEmailUser = await axios.get("user/byEmail", { email });
+    dispatch({ type: BYEMAIL_USER, payload: byEmailUser.data });
+    } catch (error) {
+    console.log(error);
+        }};
+    };
