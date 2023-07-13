8abee94230f4c95eb8beee7e9c2de86a8f13c222
leonelmunter <leonelmunter@gmail.com>
2023-07-13 13:54:23 -0300

    useClient

---
+++ front/src/customHooks/useClient.jsx
@@ -0,0 +1,29 @@
+import { useDispatch } from "react-redux";
+import React, { useState } from "react";
+import { createClient } from "../../redux/actions/actions"; //no existe ruta para esto a la fecha de creacion del componente
+
+export const useClient = () => {
+    const dispatch = useDispatch();
+    const [input, setInput] = useState({
+      name: ""
+    });
+  
+    function handleChange(e) {
+      setInput({
+        ...input,
+        [e.target.name]: e.target.value
+      });
+    }
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      dispatch(createClient(input)); //guarda con el nombre de esta action!!
+      setInput({
+        name: ""
+      });
+    };
+
+    return {
+        input, setInput, handleSubmit, handleChange
+    }
+}
