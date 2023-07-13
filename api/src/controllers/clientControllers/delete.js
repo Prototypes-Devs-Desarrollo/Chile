f00aefc5fab7e2841e979312f28ebb9c8e28d2bd
bosxch <amparocardosobosch@gmail.com>
2023-07-13 18:58:15 -0300

    cambio en data

--- api/src/controllers/clientControllers/delete.js
+++ api/src/controllers/clientControllers/delete.js
@@ -2,10 +2,10 @@ import { clients } from "../../database";
 import { response } from "../../utils";
 
 export default async (req, res) => {
-    const clientId = req.params.id;
+    const { id } = req.body;
 
-    const targetClient = await clients.findById(clientId)
+    const targetClient = await clients.findById(id)
     const deletedClient = Object.create(targetClient)
-    await clients.deleteOne({ _id: clientId });
+    await clients.deleteOne({ _id: id });
     response(res, 201, deletedClient);
 }
