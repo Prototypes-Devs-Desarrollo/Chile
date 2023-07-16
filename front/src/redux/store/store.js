d8167175833140a8e7bc45187744e0d12415d9b7
Luis Esteban Llancamil Aguilera <luis.llancamil.a@gmail.com>
2023-07-16 06:46:25 -0400

    Actualizacion

--- front/src/redux/store/store.js
+++ front/src/redux/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import reducerAuth from '../reducer/reducerAuth';
+import reducerClient from '../reducer/reducerClient';
 
 const store = configureStore({
    reducer: {
       reducerAuth,
+      reducerClient: reducerClient
    },
 });
 
