e464a3c855c10fc40a4b84512d6ac241e8cc15fd
Santy1422 <102694038+Santy1422@users.noreply.github.com>
2023-07-30 03:50:55 +0200

    importacion

--- front/src/pages/importaciones.js
+++ front/src/pages/importaciones.js
@@ -44,20 +44,24 @@ console.log(contenedoresCont)
       <>
          <div className='w-[calc(100%-256px)] h-screen overflow-y-auto p-2'>
             <Typography variant='h1'>Importaciones</Typography>
-            <Button onClick={addHandleOpenCon}>Agregar</Button>
             <div className='flex flex-row flex-wrap gap-3 justify-stretch pt-3'>
                {contenedoresCont.map((x, idx) => (
                   <Card className="w-full max-w-[16rem] cursor-pointer bg-white shadow-md rounded-lg" key={idx} color="blue" onClick={() => viewEditHandleOpenCon(x.id)}>
     <Typography variant="h6" className="text-gray-800 font-semibold mb-2">{x.nombreContenedor}</Typography>
-    <Typography className="text-gray-600 mb-1">Cant. Importaciones {x.importaciones.length}</Typography>
+    <Typography className="text-gray-600 mb-1">Cant. productos {x.importaciones.length}</Typography>
     <Typography className="text-gray-600 mb-1">fechaRDM {x?.fechaRDM}</Typography>
-    <Typography className="text-blue-600 font-medium">
-      Valor total{' '}
-      {x?.importaciones.reduce((total, ele) => total + parseInt(ele.valor.replace(/\./g, ''), 10), 0).toLocaleString('es-CL', { style: 'currency', currency: 'CLP' })}
-    </Typography>
+    <Typography className="text-gray-600 mb-1">tipo {x?.packageTipo}</Typography>
+
+
     {/* Additional content or styling can be added here */}
+    <Button onClick={viewEditHandleOpenCon}>Ver importacion</Button>
+
 </Card>
                ))}
+             
+            </div>
+            <div className='flex justify-center'>
+            <Button onClick={addHandleOpenCon}>Agregar nueva importacion</Button>
             </div>
             {addOpenCon && <AddImportacion addOpenCon={addOpenCon} addHandleOpenCon={addHandleOpenCon} />}
             {viewEditOpenCon && <ViewEditImportacion viewEditOpenCon={viewEditOpenCon} viewEditHandleOpenCon={viewEditHandleOpenCon} id={idCon} />}
