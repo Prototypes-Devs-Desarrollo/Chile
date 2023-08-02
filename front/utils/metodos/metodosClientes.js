b77a5bade0ae905d569ed53663c7c34ff635143b
ebootdpr <rodtoobe@gmail.com>
2023-08-02 01:29:59 -0300

    hotfix: doble axios innecesariom me crasheaba

--- front/utils/metodos/metodosClientes.js
+++ front/utils/metodos/metodosClientes.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 export const ClientMetodos = async ({ loading, error, success }) => {
     loading(true);
-   await axios.get('product/getAll').then((succes) => success(succes.data.payload)).catch(( err ) => error(err.message), loading(false))
-    success(response.data.payload);
-    loading(false)
+   await axios.get('product/getAll')
+   .then((response) => success(response.data.payload))
+   .catch(( err ) => error(err.message))
+   .finally(()=>loading(false))
+ 
  };
